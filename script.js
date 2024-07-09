14ff4731cf27ba9284be1a6efff22deeba9eca87
Leandro Gonçalves <lleandrogon2004@gmail.com>
2024-07-09 15:48:10 -0300

    Site da empresa Desenvolve - Ponta Verde - Maceió

---
+++ script.js
@@ -0,0 +1,45 @@
+let conteudo = document.getElementById('conteudo');
+let caixa = document.getElementById('caixa');
+
+function adicionarEvento() {
+    if (caixa.value === '') {
+        alert('Digite algo para ser adicionado!')
+    } else {
+        let tarefa = document.createElement('li');
+        tarefa.textContent = caixa.value;
+        conteudo.appendChild(tarefa);
+
+        let span = document.createElement('span');
+        span.textContent = '\u00d7';
+        tarefa.appendChild(span);
+    }
+    caixa.value = '';
+    salvar();
+}
+
+caixa.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        adicionarEvento();
+    }
+})
+
+conteudo.addEventListener('click', (el) => {
+    if(el.target.tagName == 'LI') {
+        el.target.classList.toggle('marcado');
+        salvar();
+    }
+    else if (el.target.tagName == 'SPAN') {
+        el.target.parentElement.remove();
+        salvar();
+    }
+})
+
+function salvar() {
+    localStorage.setItem('conteudos', conteudo.innerHTML);
+}
+
+function mostrar() {
+    conteudo.innerHTML = localStorage.getItem('conteudos');
+}
+
+mostrar();
